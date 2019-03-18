019c4996f3fd0a185845db3a293d53f5af8bc3ef
ruben <ruben@172.22.57.67>
2019-03-18 13:35:15 +0100

    Implementato il punto 1

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,7 +1,24 @@
 package it.polito.tdp.libretto;
 
+import java.util.*;
+
 public class Libretto {
 	
+	private List<Voto> voti;
+	
+	public Libretto() {
+		this.voti = new ArrayList<Voto>();
+	}
+	
+	/**
+	 * Aggiunge un nuovo voto al libretto
+	 * 
+	 * @param v il Voto da aggiungere
+	 */
+	
+	public void add(Voto v) {
+		voti.add(v);
+	}
 	
 
 }
